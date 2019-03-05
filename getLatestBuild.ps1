b7f50d634c7e72e05435f30f755a436a091b3536
Michael Bayles <michael.k.bayles@gmail.com>
2019-03-04 20:10:00 -0600

    Update getLatestBuild.ps1

--- getLatestBuild.ps1
+++ getLatestBuild.ps1
@@ -1,4 +1,4 @@
-$url = "$(System.TeamFoundationCollectionUri)/$(System.CollectionId)/$(System.TeamProjectId)/_apis/build/latest/$(System.DefinitionId)"
+$url = "$env:System.TeamFoundationCollectionUri/$env:System.CollectionId/$env:System.TeamProjectId/_apis/build/latest/$env:System.DefinitionId"
 Write-Host "URL: $url"
 $response = Invoke-RestMethod -Uri $url -Method Get -ContentType "application/json"
 $lastBuild = $result.id
