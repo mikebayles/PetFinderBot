69a1b58c93ffb9b5cc8939d780c1bc34c982fc50
Michael Bayles <michael.k.bayles@gmail.com>
2019-03-04 20:18:39 -0600

    Update getLatestBuild.ps1

--- getLatestBuild.ps1
+++ getLatestBuild.ps1
@@ -1,6 +1,6 @@
 $url = "$env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI$env:SYSTEM_TEAMPROJECT/_apis/build/latest/$env:SYSTEM_DEFINITIONID"
 Write-Host "URL: $url"
-$response = Invoke-RestMethod -Uri $url -Method Get -ContentType "application/json"
+$response = Invoke-RestMethod -Uri $url
 $lastBuild = $result.id
 Write-Host "LastBuild = $lastBuild"
 Write-Host "##vso[task.setvariable variable=lastBuild]$lastBuild"
