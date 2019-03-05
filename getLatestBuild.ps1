8840b0131c04b2a3b543fcecfd0f5faf03ad932b
Michael Bayles <michael.k.bayles@gmail.com>
2019-03-04 20:27:59 -0600

    Update getLatestBuild.ps1

--- getLatestBuild.ps1
+++ getLatestBuild.ps1
@@ -1,6 +1,6 @@
 $url = "$env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI$env:SYSTEM_TEAMPROJECT/_apis/build/latest/$env:SYSTEM_DEFINITIONID"
 Write-Host "URL: $url"
-$response = Invoke-RestMethod -Uri $url
+$response = Invoke-RestMethod -Uri $url -Headers @{Authorization = "Bearer $env:TOKEN"}
 Write-Host "response = $response"
 $lastBuild = $result.id
 Write-Host "LastBuild = $lastBuild"
