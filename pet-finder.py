d400af8e7495aea46459d2cb43229da8099475ea
Michael Bayles <michael.k.bayles@gmail.com>
2019-10-23 10:26:14 -0500

    Remove boto requests

--- pet-finder.py
+++ pet-finder.py
@@ -1,9 +1,8 @@
-from botocore.vendored import requests
 from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import ClientError
 import os
 import boto3
-import json
+import requests
 
 dynamodb = boto3.resource('dynamodb')
 
